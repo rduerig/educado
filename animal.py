8b53101aa2b83c5b5a73c2af7aca1c53f97a33d7
rduerig <duerig.edu@gmail.com>
2024-01-18 16:45:18 +0100

    crazy hotfix for animals, sorted cats

--- animal.py
+++ animal.py
@@ -9,7 +9,7 @@ class Animal:
         print(self.name + ": " + self.sound)
 
     def move(self):
-        print(self.name + " bewegt sich davon und macht " + self.sound)
+        print(self.name + " bewegt sich davon und sagt " + self.sound)
 
 
 class Dog(Animal):
@@ -17,7 +17,7 @@ class Dog(Animal):
     sound = "Wuff!"
 
     def move(self):
-        print(self.name + " rennt weg und macht " + self.sound)
+        print(self.name + " rennt weg und sagt " + self.sound)
 
 
 class Cat(Animal):
@@ -25,7 +25,7 @@ class Cat(Animal):
     sound = "Miau!"
 
     def move(self):
-        print(self.name + " schleicht umher und macht " + self.sound)
+        print(self.name + " schleicht umher und sagt " + self.sound)
 
 
 class Fish(Animal):
@@ -33,7 +33,7 @@ class Fish(Animal):
     sound = "Blubb!"
 
     def move(self):
-        print(self.name + " schwimmt im Kreis und macht " + self.sound)
+        print(self.name + " schwimmt im Kreis und sagt " + self.sound)
 
 
 d = Dog("Fifi")
@@ -44,10 +44,10 @@ c = Cat("Bibi")
 c.speak()
 c.move()
 
-f = Fish("Nemo")
-f.speak()
-f.move()
-
 c2 = Cat("Mimi")
 c2.speak()
 c2.move()
+
+f = Fish("Nemo")
+f.speak()
+f.move()
